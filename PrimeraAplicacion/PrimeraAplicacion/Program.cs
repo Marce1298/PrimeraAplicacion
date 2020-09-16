837768e18f8b35985936f6319c4236313264e5b3
Marce1298 <marchebordon@gmail.com>
2020-09-16 20:13:34 -0300

    Programacion

---
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeraAplicacion
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Bienvenidos al curso de C#"); //esto imprime un mensaje en la consola
+			/*Solo se pueden utilizar (mayusculas y minusculas), numeros y guiones bajos. 
+			Deven de comenzar con una letra o guión bajo, las palabras azules son palabras reservadas*/
+
+		}
+	}
+}
